e5b39c802031082e09f86f2da193b453d58bdbe5
Saad Mujawar <saadmujawar85@gmail.com>
2026-07-02 22:30:53 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "portfolio" {
+  image = docker_image.nginx.image_id
+  name  = "portfolio-container"
+
+  ports {
+    internal = 80
+    external = 8082
+  }
+}
